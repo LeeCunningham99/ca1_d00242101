199a8b6c06669ea9c58c36de3a335ca996c4e648
LeeCunningham99 <leecunningham00@gmail.com>
2023-10-10 20:54:07 +0100

    Updated the background image

--- main.cpp
+++ main.cpp
@@ -11,52 +11,34 @@
 *
 ********************************************************************************************/
 
-
-
 #include "raylib.h"
 
-//------------------------------------------------------------------------------------
-// Program main entry point
-//------------------------------------------------------------------------------------
 int main(void)
 {
     // Initialization
-    //--------------------------------------------------------------------------------------
     const int screenWidth = 1600;
     const int screenHeight = 900;
 
     InitWindow(screenWidth, screenHeight, "Midnight Motoring"); //Name of Game 
 
-    // NOTE: Be careful, background width must be equal or bigger than screen width
-    // if not, texture should be draw more than two times for scrolling effect
-    Texture2D background = LoadTexture("assets/Background.png");
-    Texture2D midground = LoadTexture("assets/tree.png");
-    //Texture2D foreground = LoadTexture("resources/cyberpunk_street_foreground.png");
+    Texture2D background = LoadTexture("assets/BackgroundImage.png");
+    //Texture2D midground = LoadTexture("assets/tree.png");
 
     float scrollingBack = 0.0f;
-    float scrollingMid = 0.0f;
-    //float scrollingFore = 0.0f;
+    //float scrollingMid = 0.0f;
 
-    SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
-    //--------------------------------------------------------------------------------------
+    SetTargetFPS(60);           
 
-    // Main game loop
-    while (!WindowShouldClose())    // Detect window close button or ESC key
+    //Main game loop
+    while (!WindowShouldClose())
     {
-        // Update
-        //----------------------------------------------------------------------------------
+        //Update
         scrollingBack -= 1.0f;
-        scrollingMid -= 0.7f;
-        //scrollingFore -= 1.0f;
+        //scrollingMid -= 0.7f;
 
-        // NOTE: Texture is scaled twice its size, so it sould be considered on scrolling
         if (scrollingBack <= -background.width*2) scrollingBack = 0;
-        if (scrollingMid <= -midground.width*2) scrollingMid = 0;
-        //if (scrollingFore <= -foreground.width*2) scrollingFore = 0;
-        //----------------------------------------------------------------------------------
+        //if (scrollingMid <= -midground.width*2) scrollingMid = 0;
 
-        // Draw
-        //----------------------------------------------------------------------------------
         BeginDrawing();
 
             ClearBackground(GetColor(0x052c46ff));
@@ -67,28 +49,21 @@ int main(void)
             DrawTextureEx(background, (Vector2){ background.width*2 + scrollingBack, 20 }, 0.0f, 2.0f, WHITE);
 
             // Draw midground image twice
-            DrawTextureEx(midground, (Vector2){ scrollingMid, 20 }, 0.0f, 2.0f, WHITE);
-            DrawTextureEx(midground, (Vector2){ midground.width*2 + scrollingMid, 20 }, 0.0f, 2.0f, WHITE);
-
-            // Draw foreground image twice
-            //DrawTextureEx(foreground, (Vector2){ scrollingFore, 70 }, 0.0f, 2.0f, WHITE);
-            //DrawTextureEx(foreground, (Vector2){ foreground.width*2 + scrollingFore, 70 }, 0.0f, 2.0f, WHITE);
+            //DrawTextureEx(midground, (Vector2){ scrollingMid, 20 }, 0.0f, 2.0f, WHITE);
+            //DrawTextureEx(midground, (Vector2){ midground.width*2 + scrollingMid, 20 }, 0.0f, 2.0f, WHITE);
 
             DrawText("SPEED - KM/H", 10, 10, 60, RED);
             DrawText("LAP - 1/3", screenWidth - 310, screenHeight - 890, 60, RED);
 
         EndDrawing();
-        //----------------------------------------------------------------------------------
     }
 
     // De-Initialization
     //--------------------------------------------------------------------------------------
     UnloadTexture(background);  // Unload background texture
-    UnloadTexture(midground);   // Unload midground texture
-    //UnloadTexture(foreground);  // Unload foreground texture
-
-    CloseWindow();              // Close window and OpenGL context
-    //--------------------------------------------------------------------------------------
+    //UnloadTexture(midground);   // Unload midground texture
+    
+    CloseWindow();      
 
     return 0;
 }
