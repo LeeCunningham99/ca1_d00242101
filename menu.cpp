ba1a1b7d84d751db1bcdb9ce75caf81b5d3bf757
LeeCunningham99 <leecunningham00@gmail.com>
2023-11-04 13:34:36 +0000

    attempted to make a main menu

---
+++ menu.cpp
@@ -0,0 +1,46 @@
+#include "menu.h"
+
+typedef enum GameScreen 
+{
+    LOGO =0, TITLE, GAMEPLAY, ENDING
+}
+GameScreen;
+int main(void)
+{
+
+//stick inside main at top
+        switch(currentScreen)
+        {
+            case TITLE:
+            {
+                // TODO: Update TITLE screen variables here!
+
+                // Press enter to change to GAMEPLAY screen
+                if (IsKeyPressed(KEY_ENTER))
+                {
+                    currentScreen = GAMEPLAY;
+                }
+            } break;
+            case GAMEPLAY:
+            {
+                // TODO: Update GAMEPLAY screen variables here!
+
+                // Press enter to change to ENDING screen
+                if (IsKeyPressed(KEY_ENTER))
+                {
+                    currentScreen = ENDING;
+                }
+            } break;
+            case ENDING:
+            {
+                // TODO: Update ENDING screen variables here!
+
+                // Press enter to return to TITLE screen
+                if (IsKeyPressed(KEY_ENTER))
+                {
+                    currentScreen = TITLE;
+                }
+            } break;
+            default: break;
+        }
+}
