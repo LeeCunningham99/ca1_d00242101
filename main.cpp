e4cc7da5ff2db1dd5e9d8ec54c5549d6f9312420
LeeCunningham99 <leecunningham00@gmail.com>
2023-11-06 19:35:10 +0000

    cleaned up main

--- main.cpp
+++ main.cpp
@@ -14,50 +14,11 @@
 #include <iostream>
 #include "player.hpp"
 #include "menu.hpp"
-//////////////////TRY TO GET PLAYER & ANIMATION STRUCTS WORKING///////////////////////
-/*
-struct Player 
-{
-    Vector2 position;
-    //float speed;
-    Texture2D texture;
-    Rectangle sourceRec;
-    float width;
-    float height;
-    int frame;
-    float runningTime;
-    const float updateTime;
-};
-*/
 
-/*
-struct Player Input (struct Player player)
-{
-    Vector2 position {100.f, 220.0f}; // Player start position
-        // Keybinds
-    if (IsKeyDown(KEY_UP) || (IsKeyDown(KEY_W)))
-        {
-            position.y -= 1.0f;
-        }
-        if (IsKeyDown(KEY_DOWN) || (IsKeyDown(KEY_S))) 
-        {
-            position.y += 1.0f;
-        }
-        if (IsKeyDown(KEY_LEFT) || (IsKeyDown(KEY_A))) 
-        {
-            position.x -= 0.2f;
-        }
-        if (IsKeyDown(KEY_RIGHT) || (IsKeyDown(KEY_D))) 
-        {
-            position.x += 1.5f;
-        }
-        return player;
-}
-*/
 using namespace std;
 int main()
 {
-    // Initialization
+    //Initialization--------------------------------------------------------------------------------------
     const int screenWidth = 1600;
     const int screenHeight = 900;
     InitWindow(screenWidth, screenHeight, "Midnight Motoring"); //Name of Game 
@@ -70,44 +31,33 @@ int main()
     player.playerPosition = {100.f, 220.0f};
     player.source = {0.f, 0.f, player.width / 3.0f, player.height}; //Source of Player's Rectangle
 
-    //Player Animation---------------------------------------------------------------------------------------
-    //player.deltaTime = GetFrameTime();
+    //Player Animation------------------------------------------------------------------------------------
     player.frame = 0;
     player.runningTime={};
     player.updateTime={1.f/10.f};
 
-    //Menu Setup
+    //Menu Setup------------------------------------------------------------------------------------------
     Menu menu;
     menu.exitWindowRequested = false;
     menu.exitWindow = false;
     menu.menuScreenWidth = screenWidth;
 
-    //Audio
+    //Audio------------------------------------------------------------------------------------------------
     InitAudioDevice(); //Initializes The Audio Devices
-    Music music = LoadMusicStream("assets/Theme.mp3");
+    Music music = LoadMusicStream("assets/Theme.mp3"); //Loads .mp3 file
     music.looping = true;
-    //float pitch = 1.0f;
     PlayMusicStream(music);
+    //float pitch = 1.0f;
     //float timePlayed = 0.0f;
     //bool pause = false;
     
-/*
-struct Player player;
-    player.texture = LoadTexture("assets/ssplayer.png");
-    player.sourceRec = {0.f, 0.f, player.width / 3.0f, (float) player.height};
-    player.position = {100.f, 220.0f};
-    player.frame = 0;
-    player.runningTime = 0.0f;
-    player.updateTime = 1.0f/10.0f;
-
-*/
     //Background
     Texture2D background = LoadTexture("assets/Background.png"); //Background Image
     float scrollingBack = 0.0f;
     //float scrollingMid = 0.0f;
     SetTargetFPS(60);           
-    //Main game loop
-    //while (!WindowShouldClose()) //Exits Application when esc is pressed
+    //Main game loop---------------------------------------------------------------
+
     while (!menu.exitWindow) //Displays an Exit option when esc is pressed
     {
         player.deltaTime = GetFrameTime();
@@ -115,29 +65,8 @@ struct Player player;
         UpdateMusicStream(music);
         //Update
         scrollingBack -= 1.0f;
-        //scrollingMid -= 0.7f;
         if (scrollingBack <= -background.width*2) scrollingBack = 0;
-        //if (scrollingMid <= -midground.width*2) scrollingMid = 0;
-        //float deltaTime = GetFrameTime(); //Time inbetween frame updates
-        //Movement for player. Arrow keys or WASD keys work
-        /*
-        if (IsKeyDown(KEY_UP) || (IsKeyDown(KEY_W)))
-        {
-            position.y -= 1.0f;
-        }
-        if (IsKeyDown(KEY_DOWN) || (IsKeyDown(KEY_S))) 
-        {
-            position.y += 1.0f;
-        }
-        if (IsKeyDown(KEY_LEFT) || (IsKeyDown(KEY_A))) 
-        {
-            position.x -= 0.2f;
-        }
-        if (IsKeyDown(KEY_RIGHT) || (IsKeyDown(KEY_D))) 
-        {
-            position.x += 1.5f;
-        }
-        */
+
         BeginDrawing();
         ClearBackground(BLACK);
 
@@ -146,28 +75,11 @@ struct Player player;
             // NOTE: Texture is scaled twice its size
             DrawTextureEx(background, (Vector2){ scrollingBack, 0 }, 0.0f, 0.79f, WHITE);
             DrawTextureEx(background, (Vector2){ background.width*2 + scrollingBack, 20 }, 0.0f, 2.0f, WHITE);
-
-            // Draw midground image twice
-            //DrawTextureEx(midground, (Vector2){ scrollingMid, 20 }, 0.0f, 2.0f, WHITE);
-            //DrawTextureEx(midground, (Vector2){ midground.width*2 + scrollingMid, 20 }, 0.0f, 2.0f, WHITE);
             
           //Player Animation----------------------------------------------------------------------------------------------------------------------------
           player.animation();
-          /*
-            runningTime += deltaTime;
-            if (runningTime >= updateTime)
-            {
-                runningTime = 0.f;
-                player.source.x = (float) frame * player.source.width;
-                frame++;
-                if (frame > 3)
-                {
-                    frame = 0;
-                }
-            }
-            */
+         
         //Player Texture Drawn--------------------------------------------------------------------------------------------------------------------------
-        //DrawTextureRec(player.playerImage, player.source, playerPosition, WHITE);
         player.drawPlayer();
         //Text 
             DrawText("SPEED - KM/H", 10, 10, 60, RED);
@@ -176,13 +88,12 @@ struct Player player;
 //menu--------------------------------------------------------------------------------------------------------------------------------------------
         menu.menuExit();
         menu.gameClose();
+
         EndDrawing();
     }
     // De-Initialization
     //--------------------------------------------------------------------------------------
     UnloadTexture(background);  // Unload background texture
-    //UnloadTexture(midground);   // Unload midground texture
-    //UnloadTexture(playerImage); // Unload player's spritesheet texture
     player.unloadTexture();
     UnloadMusicStream(music);
     
